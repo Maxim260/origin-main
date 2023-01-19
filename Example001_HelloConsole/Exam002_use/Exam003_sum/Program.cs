6bfe52e90b29ae716deed3c74c2e690318fb3b54
Максим <maximka9i@vk.com>
2023-01-19 18:19:19 +1000

    if,else

--- Example001_HelloConsole/Exam002_use/Exam003_sum/Program.cs
+++ Example001_HelloConsole/Exam002_use/Exam003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 456;
-int numberB = 4568897;
+﻿int numberA = new Random().Next(1,89756);//0 1 2 3 4...89755
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,678932);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
---
+++ Example001_HelloConsole/exam005_if/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "max")
+{
+    Console.WriteLine("Максим,идём поссым.))");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
